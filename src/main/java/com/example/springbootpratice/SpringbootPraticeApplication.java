60ad0c1bb9c90702b28bce3fe3abf0457f20a50b
lala9663 <lala96632040@gmail.com>
2023-02-16 16:39:26 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootpratice/SpringbootPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPraticeApplication.class, args);
+    }
+
+}
